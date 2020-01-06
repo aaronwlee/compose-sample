461363ab539be8bc8d643c9abfbb96438cdf0928
Tony Park <tonypark0403@gmail.com>
2020-01-05 22:40:08 -0500

    pushed first image for nodejs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # docker
+
 Test docker
+
+## image
+
+- [x] nodejs-test PORT:9000
+
+## reference
+
+```
+docker build -t "nodejs-test" .
+docker run --env-file .env -p 8000:9000 nodejs-test
+docker login
+docker build -t <account>/docker-to-kubernetes .
+docker image ls
+docker push <account>/docker-kubernetes
+```
---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9999;
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:${process.env.PORT}`);
+});
